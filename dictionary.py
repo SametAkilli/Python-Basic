94a5f9b2e3ff02a7ad0ba6695fa044320492383a
Samet <113308717+SametAkilli@users.noreply.github.com>
2023-02-22 13:34:04 +0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+list = [25,3,8,11]
+
+list.pop() #delete last value
+list.insert(1,31) #add number 31 before the number 1 value
+list.sort() #sort the list
+result =len(list) 
+print(list)
+print(result)
