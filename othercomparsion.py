1e8759cad5af20c121f232017c4dceff1d681920
Samet <113308717+SametAkilli@users.noreply.github.com>
2023-02-22 19:05:15 +0300

    Add files via upload

---
+++ othercomparsion.py
@@ -0,0 +1,16 @@
+x = y = [1,2,3]
+z = [1,2,3]
+result = (x==y)
+result1 = (x==z)
+result2 = (x is y)
+result3 = (x is z)
+print(result)
+print(result1)
+print(result2)
+print(result3)
+
+
+x = ['apple','banana']
+
+print('banana' in x)
+print('pinaplle' in x)
